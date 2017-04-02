df474b4ab6143757f591681bd7c0e6486183f2a8
Magnus Runesson <M.Runesson@gmail.com>
2017-04-02 20:58:04 +0200

    Parallel requests.

--- src/main/java/org/linuxalert/lab/grpc/client/FeignClient.java
+++ src/main/java/org/linuxalert/lab/grpc/client/FeignClient.java
@@ -7,10 +7,12 @@ import org.linuxalert.lab.grpc.model.Data;
 import org.linuxalert.lab.grpc.model.RequestData;
 
 import java.util.Date;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 public class FeignClient {
 
-  public static void main(String[] args) throws Exception {
+  public static void run() {
     long startTime = new Date().getTime();
     BootClient client = Feign
         .builder()
@@ -23,6 +25,20 @@ public class FeignClient {
     }
     long stopTime = new Date().getTime();
     System.out.println(stopTime-startTime);
+  }
+
+  public static void main(String[] args) throws Exception {
+    Executor e = new ScheduledThreadPoolExecutor(10);
+    e.execute(FeignClient::run);
+    e.execute(FeignClient::run);
+    e.execute(FeignClient::run);
+    e.execute(FeignClient::run);
+    e.execute(FeignClient::run);
+    e.execute(FeignClient::run);
+    e.execute(FeignClient::run);
+    e.execute(FeignClient::run);
+    e.execute(FeignClient::run);
+    e.execute(FeignClient::run);
   }
 
 }
